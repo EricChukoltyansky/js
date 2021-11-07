2c9b81b8a9e3f666abf199118b188244e4d94774
EricChukoltyansky <erik.chukoltyansky@gmail.com>
2021-11-07 13:38:48 +0200

    finished

---
+++ js 101/2.4/index.js
@@ -0,0 +1,16 @@
+// var today = new Date();
+// var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+
+let today = new Date();
+
+const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+function todayDate() {
+    return 'Today is ' + days[today.getDay()] + " the " +today.getDate() + ' of ' + monthNames[today.getMonth()] + ' , ' + today.getFullYear()
+}
+
+console.log(todayDate())
