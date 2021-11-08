d4569c4ede24622ca60cd8ef1cc449f62dbd7a17
EricChukoltyansky <erik.chukoltyansky@gmail.com>
2021-11-08 10:46:49 +0200

    Finished

--- js 101/3.1/3.1.function_expressions.js
+++ js 101/3.1/3.1.function_expressions.js
@@ -1,13 +1,23 @@
-// From function declarations to function expressions
+/*
+    JavaScript - Declaring Functions
+    The following exercise contains the following subjects:
+        * functions
+    
+    Instructions
+        * Please reformat the following function expressions to IIFE functions.
+        * Please reformat the following function declarations in two ways, explicit return and implicit return functions.
+    Submit the file to Hive
+*/
+
+// From function declarations to explicit and implicit return functions (one of each).
 // function welcome() {
 //     let welcome = 'Welcome to Appleseeds Bootcamp!';
 //     return welcome;
 // }
 
-const welcome = function() {
-    let welcome = 'Welcome to appleseeds Bootcamp!';
-    return welcome;
-}
+const welcome = () => {
+  "Welcome to appleseeds Bootcamp!";
+};
 
 // function power(a) {
 //     let myNumber = a;
@@ -15,40 +25,19 @@ const welcome = function() {
 //     return result;
 // }
 
-const power = function(a) {
-    let myNumber = a;
-    let result = Math.pow(myNumber, 2);
-    return result;
-}
+const power = (a) => {
+  Math.pow(a, 2);
+};
 
-// function add(a, b = 5) {
-//     let myNumber = a;
-//     let sum = myNumber + b;
-//     return sum;
-// }
-
-const add = function(a, b = 5) {
-    let myNumber = a;
-    let sum = myNumber + b;
-    return sum;
-}
-
-
-// From function expressions to function declarations
-// const hello = () => "Hello!";
-
-function hello() {
-    return 'Hello';
-}
-
+// From function expressions to IIFE functions.
 // const squareRoot = a => Math.sqrt(a);
 
-function squareroote(a) {
-    Math.sqrt(a)
+function squareRoot(a) {
+  return Math.sqrt(a);
 }
 
 // const randomNumbers = (a, b) => Math.random() * (a - b) + b;
 
-function randomNumbers(a,b) {
-    Math.random() * (a-b) + b;
+function randomNumbers(a, b) {
+  return Math.random() * (a - b) + b;
 }
