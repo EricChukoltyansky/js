bba926be2d5dfe913a35e2519b85a293c5e52320
EricChukoltyansky <kazohelmet@gmail.com>
2021-11-08 13:13:09 +0200

    finished

---
+++ js 101/4.1/4.1.number.js
@@ -0,0 +1,17 @@
+
+
+let numSiblings = window.prompt("How many siblings do you have?");
+
+if(numSiblings === '1') {
+    alert('1 sibling')
+}
+
+else if(numSiblings > 1) {
+    alert('More than 1 sibling')
+}
+
+else {
+    alert('No sibling')
+}
+
+numSiblings();
