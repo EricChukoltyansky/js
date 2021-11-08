f7513bd0e3c82f4d3ea1e18da05d4e15b36d8daf
EricChukoltyansky <kazohelmet@gmail.com>
2021-11-08 11:47:31 +0200

    Finished

---
+++ js 101/3.3/3.3.func_decl_vs_exp.js
@@ -0,0 +1,20 @@
+let populationChina = 1441000000;
+let populationPakistan = 22900000;
+let populationIndia = 13000000;
+
+function percentageOfWorld1(a) {
+    return (a/7900)*100;
+}
+
+let percentageOfWorld2 = function(a) {
+    return (a/7900)*100;
+  }
+
+
+let china = console.log(percentageOfWorld1(populationChina));
+let india = console.log(percentageOfWorld1(populationIndia));
+let pakistan = console.log(percentageOfWorld1(populationPakistan));
+
+let china2 = console.log(percentageOfWorld2(populationChina));
+let india2 = console.log(percentageOfWorld2(populationIndia));
+let pakistan2 = console.log(percentageOfWorld2(populationPakistan));git 
