e5d80647d23dced5726b67f8be8bcb5eed666e28
EricChukoltyansky <kazohelmet@gmail.com>
2021-11-09 11:48:32 +0200

    Finished

---
+++ js 101/5.1/5.1-country.js
@@ -0,0 +1,12 @@
+
+
+let language, isIsland, population, country;
+
+const countryToLiveIn = function(language, isIsland, population, country) {
+  if(language = 'English' && (isIsland = false) && population < 50000000 && (country = 'Denmark') ) {
+      return "Denmark is the country for you"
+  }
+
+  else {"Denmark is not the country for you"}
+}
+
