aa4f7b1a75a9b5959c23fbfc4d26a62e9240f043
EricChukoltyansky <kazohelmet@gmail.com>
2021-11-09 14:28:25 +0200

    Finished

---
+++ js 101/6.2/6.2.basics.js
@@ -0,0 +1,51 @@
+const people = ["Greg", "Mary", "Devon" ,"James"];
+
+// 1 
+for(let i of people) {
+    console.log(people[i])
+}
+
+// 2
+let removedElem = people.shift();
+console.log(removedElem)
+
+// 3
+let removedElem2 = people.pop();
+console.log(removedElem2);
+
+// 4
+let addElem = people.unshift("Matt");
+console.log(addElem);
+
+// 5
+let addElem2 = people.push("Eric");
+console.log(addElem2);
+
+// 6
+for(let i of people){
+    if(people[i] === "Mary") {
+        break;
+    }
+}
+
+// 7
+let newArr = people.slice(2,4)
+console.log(newArr);
+
+// 8
+let index = people.indexOf('Mary');
+console.log(index);
+
+// 9
+let index2 = people.indexOf('Foo');
+console.log(index2);
+
+// 10
+let spliced = people.splice(2,2,"Elizabeth", "Artie")
+console.log(spliced)
+
+// 11
+let withBob = "Bob";
+let concatenated = people.concat(withBob);
+console.log(concatenated)
+
