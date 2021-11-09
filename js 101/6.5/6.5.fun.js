ea5954ff6bdb19478dc398b6aeee08e91bad6a1a
EricChukoltyansky <kazohelmet@gmail.com>
2021-11-09 15:53:13 +0200

    Finished

--- js 101/6.5/6.5.fun.js
+++ js 101/6.5/6.5.fun.js
@@ -1,15 +1,15 @@
 // 1
 let array = [];
-let filledArr = new Array[100].fill('same')
-console.log(filledArr)
+let filledArray = new Array(100).fill({'hello':'goodbye'});
+console.log(filledArray)
 
 // 2
-let filledArr2 = Array.from(Array(10).keys())
+let filledArr2 = Array.from(Array(100).keys())
 console.log(filledArr2)
 
 // 3
-var obj = {"1":5,"2":7,"3":0,"4":0,"5":0,"6":0,"7":0,"8":0,"9":0,"10":0,"11":0,"12":0};
-var result = Object.values;
+var obj = {"1":5,"2":7,"3":4,"4":6,"5":8,"6":9,"7":0,"8":0,"9":0,"10":0,"11":0,"12":0};
+var result = Object.values(obj);
 console.log(result);
 
 // 4
@@ -21,7 +21,9 @@ console.log(
 Array.isArray(array);
 
 // 6
+let arr= [];
 const newArr = arr.slice();
 
 // 7
-const newArr = [...arr];
+let arr= []
+const newArr2 = arr;
