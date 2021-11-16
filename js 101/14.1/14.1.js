93921c54f3d09ae530d4e199294ad0a127f2776e
EricChukoltyansky <kazohelmet@gmail.com>
2021-11-16 11:24:00 +0200

    Finished

---
+++ js 101/14.1/14.1.js
@@ -0,0 +1,35 @@
+const grades = [10, 20, 30, 90, 50, 60, 70, 80, 40];
+
+let max = grades.reduce((max, currVal) => {
+  if (max < currVal) return currVal;
+  return max;
+});
+
+console.log(max);
+
+let evenSum = grades
+  .filter((num) => !(num % 2))
+  .reduce((a, b) => {
+    return a + b;
+  });
+
+console.log(evenSum);
+
+// const arr = [129, 139, 155, 176];
+
+// sum all the elements of the array
+// const sum = arr.reduce(function (accumulator, currentValue){
+//   return accumulator + currentValue;
+// }, 0);
+
+// const average = sum / arr.length;
+
+// console.log(average);
+
+let sum = grades.reduce((total, currVal) => {
+  return total + currVal;
+});
+
+let average = sum / grades.length;
+
+console.log(average);
