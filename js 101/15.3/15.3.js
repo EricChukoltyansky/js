c58fb9bf8e47bdedb24d5b9858a14f85be77b6d9
EricChukoltyansky <kazohelmet@gmail.com>
2021-11-17 11:37:42 +0200

    Finished

---
+++ js 101/15.3/15.3.js
@@ -0,0 +1,11 @@
+arr1 = [1,2,3,4,5,6,7,8,9,10];
+
+function getSumOfEven(arr){
+     
+     let sum = arr[0] +arr[2] + arr[4] + arr[6] + arr[8];
+     return sum;
+   }
+
+   console.log(getSumOfEven(arr1));
+
+// 1. There's no arr[10] in an array that's length is 10
