b1cd2a1cf813b6c899239c08e50a2fc945d0b2fa
EricChukoltyansky <kazohelmet@gmail.com>
2021-11-17 13:41:04 +0200

    Finished

--- js 101/16.1/16.1.js
+++ js 101/16.1/16.1.js
@@ -1,13 +1,31 @@
 function funcA() {
-    
-    console.log(a);
-    console.log(foo());
-    var a = 1;
-    function foo() {
+  console.log(a);
+  console.log(foo());
+  var a = 1;
+  function foo() {
     return 2;
-    }
-   }
-   funcA();
+  }
+}
+funcA();
 
 //  undefined, console.log(a) prints out the var, but since it is hoisted under its console.log it won't print out.
 //  2, funcA() prints out foo() which prints 2 in its turn.
+
+var fullName = "John Doe";
+var obj = {
+  fullName: "Colin Ihrig",
+  prop: {
+    fullName: "Aurelio De Rosa",
+    getFullName: function () {
+      return this.fullName;
+    },
+  },
+};
+console.log(obj.prop.getFullName());
+var test = obj.prop.getFullName();
+console.log(test);
+
+// console.log(obj.prop.getFullName()) will print out Aurelio De Rosa
+// console.log(test()); will print out Aurelio De Rosa since it will get inside the method getFullName => return obj.prop.fullname => "Aurelio De Rosa"
+
+
