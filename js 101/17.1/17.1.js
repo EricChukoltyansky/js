e95eef44c29191797b7cfcf31ec9aa7bd9017441
EricChukoltyansky <kazohelmet@gmail.com>
2021-11-17 15:41:59 +0200

    Finished

--- js 101/17.1/17.1.js
+++ js 101/17.1/17.1.js
@@ -1,13 +1,33 @@
-var b = 1;
-function someFunction(number) {
- function otherFunction(input) {
- return b;
+// var b = 1;
+// function someFunction(number) {
+//  function otherFunction(input) {
+//  return b;
+//  }
+//  b = 5;
+//  return otherFunction;
+// }
+// console.log(firstResult = someFunction(-20));
+// console.log(result = firstResult(8))
+
+// Will always return 5 and otherFunction since number and input don't play a part in the function
+
+// var a = 1;
+// function b2() {
+//  a = 10;
+//  return;
+//  function a() { }
+// }
+// b2();
+// console.log(a);
+
+// Will always print out just 1, because var a = 1, and b2() has no specific return
+
+let i;
+for (i = 0; i < 3; i++) {
+ const log = () => {
+ console.log(i);
  }
- b = 5;
- return otherFunction;
+ setTimeout(log, 100);
 }
-console.log(firstResult = someFunction(-20));
-console.log(result = firstResult(8))
 
-// Will always return 5 and otherFunction since 
-
+// Prints out 3, 3, 3 because let i is globally defined, we can make print 0,1,2 if we define it inside the loop function
