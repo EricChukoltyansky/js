0bb33f9dcfbda7261fed17cff40ce513b582ab98
EricChukoltyansky <kazohelmet@gmail.com>
2021-12-07 14:29:59 +0200

    Joke api

---
+++ js 101/28.1/script.js
@@ -0,0 +1,28 @@
+const jokebtn = document.querySelector(`.getJoke`);
+const jokeHolder = document.querySelector(`.joke p`);
+const titleHolder = document.querySelector(".title p");
+const jokeButtonText = jokebtn.querySelector(".btn-text");
+
+const baseUrl = "https://api.jokes.one/jod";
+
+const getJoke = () => {
+  jokeHolder.textContent =
+    "Please wait for the joke, hope it's good enough to worth the wait";
+  fetch(baseUrl, {
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then(
+      (data) => (titleHolder.textContent = data.contents.jokes[0].joke.title)
+    )
+    .then((data) => (jokeHolder.textContent = data.contents.jokes[0].joke.text))
+    .catch((err) => (jokeHolder.textContent = "Try again"));
+};
+
+const clickHandler = () => {
+  getJoke();
+};
+
+jokebtn.addEventListener("click", clickHandler);
