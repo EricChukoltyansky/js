499c7d552653d51532dbed64038638fffa3d9eb7
EricChukoltyansky <kazohelmet@gmail.com>
2021-12-07 18:47:02 +0200

    Async await

---
+++ js 101/29.1/script.js
@@ -0,0 +1,48 @@
+const getIDs = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve([532, 543, 753, 1, 5]);
+    }, 1500);
+  });
+
+const getRecipe = (recipeID) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(
+      (ID) => {
+        const recipe = {
+          title: "Fresh tomato pasta",
+          publisher: "Pinchas Hodadad",
+        };
+        resolve(`${ID}: ${recipe.title}`);
+      },
+      1500,
+      recipeID
+    );
+  });
+};
+
+getIDs()
+  .then((IDs) => {
+    console.log(IDs);
+    return getRecipe(IDs[2]);
+  })
+  .then((recipe) => {
+    console.log(recipe);
+  })
+  .catch((error) => {
+    
+  });
+
+async function getSomeMoreIds() {
+ try {
+    let Ids = await getIDs();
+    let recipe = await getRecipe(Ids[2]);
+    console.log(recipe)
+ }
+ catch {
+    console.log("It is an error!");
+ }
+  
+}
+
+getSomeMoreIds()
