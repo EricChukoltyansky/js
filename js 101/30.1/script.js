8a0ba5614f11747463a2e0cf62144ef0eb03b509
EricChukoltyansky <kazohelmet@gmail.com>
2021-12-08 11:39:41 +0200

    git api

---
+++ js 101/30.1/script.js
@@ -0,0 +1,29 @@
+const button = document.querySelector("button");
+let userName = document.querySelector("section .user-name");
+let image = document.querySelector("section .image-container");
+let repoNumber = document.querySelector("section .repo-number");
+
+let getFetch = async () => {
+  const val = document.querySelector("input").value;
+  console.log(val);
+  userName.textContent = "Searching.. Please Hold you horses";
+  repoNumber.textContent = "Chill mate, still looking";
+  const res = await fetch(`https://api.github.com/users/${val}`, {
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  let data = await res.json();
+  console.log(data);
+  try {
+    userName.textContent = data.login;
+    image.style.backgroundImage = `url("${data.avatar_url}")`;
+    repoNumber.textContent = data.public_repos;
+  } catch {
+    userName.textContent = "No such user";
+    image.style.backgroundImage = "No such user image";
+    repoNumber.textContent = "No such user repos";
+  }
+};
+
+button.addEventListener("click", getFetch);
