501a04c9d5b60380770708a56483b381699a4136
EricChukoltyansky <kazohelmet@gmail.com>
2022-12-21 16:31:33 +0200

    9.4 renew

--- js 101/9.4/9.4.steps.js
+++ js 101/9.4/9.4.steps.js
@@ -10,6 +10,25 @@ let hashTag = (N) => {
     }
     console.log(str);
   }
+  return;
 };
 
-console.log(hashTag(20));
+console.log(hashTag(5));
+
+function steps(n) {
+  let result = [];
+  for (let i = 0; i <= n; i++) {
+      result[i] = ''
+      for (let j = n; j > i; j--) {
+          result[i] += ('#');
+      }
+      for (let t = n - result[i].length; t > 0; t--) {
+          result[i] += ' ';
+      }
+
+  }
+  result.pop()
+  return result.reverse().join('\n')
+}
+
+console.log(steps(5));
